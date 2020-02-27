a1cb8ca60ae2c4d370839d9eef87fd6a4285c59a
Chee <tobyl19@hotmail.com>
2020-02-27 12:09:51 +0900

    change URL

--- src/utils/siteInit.js
+++ src/utils/siteInit.js
@@ -1,4 +1,4 @@
 export const SiteAPI = {  
-    rootURI: 'http://127.0.0.1:8000/',
-    imgStroge: 'http://localhost:8000/storage/'
+    rootURI: 'http://testserver.vanuatu.jbgcore.com/',
+    imgStroge: 'http://testserver.vanuatu.jbgcore.com/storage/'
 };
